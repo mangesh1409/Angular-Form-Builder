abaf035976b6f63622db41632b0a9f20af35d8c4
Mangesh Jadhav <149mpj@gmail.com>
2020-07-17 00:02:56 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,40 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'FormBuilder';
+  data;
+
+  constructor(private fb:FormBuilder){}
+
+  profileForm=this.fb.group({
+    nfgname: [''],
+    nfgemail: [''],
+    nfgaddress:this.fb.group({
+      street: [''],
+      city: [''],
+      state: [''],
+    })
+  });
+
+  Submitdata(profileForm){
+    // console.log(profileForm.value);
+    this.data = profileForm.value;
+    // console.log(profileForm.controls);
+    console.log(profileForm.controls.nfgname.value);
+    console.log(profileForm.controls.nfgemail.value);
+    console.log(profileForm.nfgaddress.controls.street.value);
+    // console.log(profileForm.controls.city.value);
+    // console.log(profileForm.controls.state.value);
+
+}
+  updateProfile() {
+    this.profileForm.patchValue({
+      nfgname: 'Rohan',
+      nfgemail: '234@gmail.com',
+      nfgaddress: {
+        street: '123 Drew',
+        city: 'Mumbai',
+        state: 'Maharashtra'
+      }
+    });
+  }
 }
